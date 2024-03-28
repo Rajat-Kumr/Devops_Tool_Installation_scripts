0cdff85f7182149af9e7adf0c56208d55f27152e
Rajat-Kumr <146103913+Rajat-Kumr@users.noreply.github.com>
2024-03-28 08:38:23 +0530

    Update Jenkins_war_deployment.sh

--- Jenkins_war_deployment.sh
+++ Jenkins_war_deployment.sh
@@ -9,3 +9,31 @@ chmod 755 -R /opt/tomcat
 wget https://updates.jenkins.io/download/war/2.397/jenkins.war
 cp jenkins.war /opt/tomcat/webapps
 yum install git -y
+
+
+
+
+######################################################################################################
+#
+#                                               UBUNTU
+######################################################################################################
+
+
+#!/bin/bash
+sudo apt update
+sudo apt install default-jdk
+apt install unzip
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.86/bin/apache-tomcat-9.0.86.zip
+unzip apache-tomcat-9.0.86.zip
+mv apache-tomcat-9.0.86 /opt/tomcat
+chmod -R 755 /opt/tomcat/
+/opt/tomcat/bin/startup.sh
+wget https://updates.jenkins.io/download/war/2.431/jenkins.war
+cp jenkins.war /opt/tomcat/webapps/
+
+
+#######################################################################################################
+
+
+
+
